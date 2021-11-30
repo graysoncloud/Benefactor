64a652840de058702c070994eab07dfde9c91819
Booker <bookerjmartin@gmail.com>
2021-11-29 18:22:53 -0800

    Windows + Fixed Wall Transitions

--- Main/Assets/Scripts/Objects/Wall.cs
+++ Main/Assets/Scripts/Objects/Wall.cs
@@ -2,33 +2,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Wall : InteractableObject
 {
     public Sprite front;
+    public Sprite frontWindow;
+    public Sprite frontWindowPlanter;
     public Sprite frontLeft;
     public Sprite frontRight;
+    public Sprite frontUpDownLeft;
+    public Sprite frontUpDownRight;
     public Sprite frontUp;
     public Sprite back;
     public Sprite backLeft;
     public Sprite backRight;
     public Sprite backUp;
     public Sprite backDown;
+    public Sprite DownLeftBackRightFront;
+    public Sprite DownLeftFrontRightBack;
     public Sprite backUpLeft;
     public Sprite backUpRight;
     public Sprite backUpDownLeft;
     public Sprite backUpDownRight;
     public Sprite backUpDownLeftRight;
+    public Sprite UpDownLeftBackRightFront;
+    public Sprite UpDownLeftFrontRightBack;
     public Sprite side;
 
     private int wallIndex;
     private bool isFront = false;
 
-    // Start is called before the first frame update
+    //Start is called before the first frame update
     void Start()
     {
-        base.Start();
         SetSprite();
+        base.Start();
+    }
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     void SetSprite()
@@ -37,6 +52,9 @@ public class Wall : InteractableObject
         bool isDown = false;
         bool isLeft = false;
         bool isRight = false;
+        bool isRightFront = false;
+        bool isLeftFront = false;
+        bool nearWindow = false;
        
             Collider2D[] hitColliders = Physics2D.OverlapCircleAll(((Vector2) transform.position) + new Vector2(0, 1), 0.5f);
             foreach (Collider2D hitCollider in hitColliders) {
@@ -50,25 +68,37 @@ public class Wall : InteractableObject
             }
             hitColliders = Physics2D.OverlapCircleAll(((Vector2) transform.position) + new Vector2(-1, 0), 0.5f);
             foreach (Collider2D hitCollider in hitColliders) {
-                if (hitCollider.GetComponent<Wall>() != null || hitCollider.GetComponent<Door>() != null)
+                Wall wall = hitCollider.GetComponent<Wall>();
+                if (wall != null || hitCollider.GetComponent<Door>() != null) {
                     isLeft = true;
+                    if (wall != null) {
+                        isLeftFront = wall.GetInFront();
+                        nearWindow = wall.IsWindow() ? true : nearWindow;
+                    }
+                }
             }
             hitColliders = Physics2D.OverlapCircleAll(((Vector2) transform.position) + new Vector2(1, 0), 0.5f);
             foreach (Collider2D hitCollider in hitColliders) {
-                if (hitCollider.GetComponent<Wall>() != null || hitCollider.GetComponent<Door>() != null)
+                Wall wall = hitCollider.GetComponent<Wall>();
+                if (wall != null || hitCollider.GetComponent<Door>() != null) {
                     isRight = true;
+                    if (wall != null) {
+                        isRightFront = wall.GetInFront();
+                        //nearWindow = wall.IsWindow() ? true : nearWindow;
+                    }
+                }
             }
 
         if (isUp && isDown && isLeft && isRight)
-            spriteRenderer.sprite = backUpDownLeftRight;
+            spriteRenderer.sprite = isRightFront ? UpDownLeftBackRightFront : isLeftFront ? UpDownLeftFrontRightBack : backUpDownLeftRight;
         else if (isUp && isDown && isLeft)
-            spriteRenderer.sprite = backUpDownRight;
+            spriteRenderer.sprite = isLeftFront ? frontUpDownRight : backUpDownRight;
         else if (isUp && isDown && isRight)
-            spriteRenderer.sprite = backUpDownLeft;
+            spriteRenderer.sprite = isRightFront ? frontUpDownLeft : backUpDownLeft;
         else if (isUp && isLeft && isRight)
             spriteRenderer.sprite = isFront ? frontUp : backUp;
         else if (isDown && isLeft && isRight)
-            spriteRenderer.sprite = backDown;
+            spriteRenderer.sprite = isRightFront ? DownLeftBackRightFront : isLeftFront ? DownLeftFrontRightBack : backDown;
         else if (isDown && isRight)
             spriteRenderer.sprite = backLeft;
         else if (isDown && isLeft)
@@ -78,12 +108,20 @@ public class Wall : InteractableObject
         else if (isUp && isLeft)
             spriteRenderer.sprite = isFront ? frontRight : backUpRight;
         else if (isRight && isLeft)
-            spriteRenderer.sprite = isFront ? front : back;
+            spriteRenderer.sprite = isFront ? ((nearWindow || Random.Range(0,3) > 0) ? front : Random.Range(0,2) == 0 ? frontWindow : frontWindowPlanter) : back;
         else if (isUp && isDown)
             spriteRenderer.sprite = side;
     }
 
     public void IsFront(bool inFront = true) {
         isFront = inFront;
+    }
+
+    public bool GetInFront() {
+        return isFront;
+    }
+
+    public bool IsWindow() {
+        return spriteRenderer.sprite == frontWindow || spriteRenderer.sprite == frontWindowPlanter;
     }
 }
